7b5d8fa99157153a47c535ea69a275af3073719e
Mohamed Abdelkader <mohamedashraf123@gmail.com>
2021-06-06 00:15:09 +0300

    Update optimization.py

--- octune/optimization.py
+++ octune/optimization.py
@@ -30,8 +30,9 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 import numpy as np
+from numpy.lib.function_base import select
 import scipy as sp
-import scipy.signal
+# import scipy.signal
 
 class BackProbOptimizer:
     """Implements backprobagation techniques to optimize a linear controller for an unkown system, given system & controller output data.
@@ -57,9 +58,24 @@ class BackProbOptimizer:
         self._dL_da=None    # Partial derivatives w.r.t to controller denomentator's coeffs, a
         self._dL_db=None    # Partial derivatives w.r.t to controller numerator's coeffs, b
 
+        # Optimization objective function
+        self._objective=None
+        # Error array between reference signal and systen output
+        self._error=None
+
+
+        # Optimization learning rate
+        self._alpha=0.001
 
         # TODO ADAM optimizer variables
         self._use_adam=True # True: Use  ADAM algorithm (recommended); False: Use regular gradient descent aglrithm
+        self._beta1=0.9
+        self._beta2=0.999
+        self._eps=10e-8
+        self._vda=0.0
+        self._sda=0.0
+        self._vdb=0.0
+        self._sdb=0.0
 
     def backward(self):
         """Performs backward probagation steps.
@@ -102,8 +118,54 @@ class BackProbOptimizer:
             return False
 
         # TODO implement the backward probagation steps
+        self._r = np.reshape(self._r, (len(self._r),1) )
+        self._u = np.reshape(self._u, (len(self._u),1) )
+        self._y = np.reshape(self._y, (len(self._y),1) )
 
-        # Compute error
+        # Compute objective
+        self._objective = 0.5 * np.linalg.norm(self._r - self._y)**2
+
+        # compute error
+        self._error = self._r - self._y
+
+        # Partial derivatives w.r.t system output, y
+        self._dL_dy = -1.0 * (self._r - self._y)
+
+        # Partial derivatives w.r.t controller output, u
+        # This is computed numerically using finite differences
+        u_shifted = np.roll(self._u,1)
+        u_shifted[0]=0.0
+        y_shifted = np.roll(self._y,1)
+        y_shifted[0]=0.0
+        dy_du = y_shifted/u_shifted
+        # Handle inf/nan elements (for now, replace nan by 0, inf by a large number and copy sign)
+        dy_du = np.nan_to_num(dy_du)
+
+        # Partial derivatives w.r.t conroller numerator coeffs, b
+        dtype_np = self._error.numpy().dtype
+        d0_np = np.array([1.0], dtype=dtype_np)
+        n_b = len(self._b) # Number of b coeffs
+        T = len(self._error) # Number of time steps
+        # Compute forward sensitivities w.r.t. the controller's b_i parameters
+        db = np.zeros_like(self._error, shape=(T,n_b)) # [T, n_b]
+        db[:, 0] = sp.signal.lfilter(d0_np, self._a, self._error)
+        for idx_coeff in range(1, n_b):
+            db[idx_coeff:, idx_coeff] = db[:-idx_coeff, 0]
+
+        self._dL_db = np.sum(self._dL_dy * dy_du * db, axis=0)
+
+        # Partial derivatives w.r.t conroller denominator coeffs, a
+        # Compute forward sensitivities w.r.t. the controller's a_i parameters
+        n_a = len(self._a) # Number of a coeffs
+        d1_np = np.array([0.0, 1.0], dtype=dtype_np)
+        da = np.zeros_like(self._error, shape=(T,n_a-1)) # [T, n_a-1], remove the 1st element since it's constant, a[0]=1
+        da[:, 0] = sp.signal.lfilter(d1_np, self._a, -self._u)
+        for idx_coeff in range(1, n_a-1):
+            da[idx_coeff:, idx_coeff] = da[:-idx_coeff, 0]
+
+        # Note that the gradient dL_da does not include the one for a[0], because it's constant, a[0]=1
+        # dL_da[0] is the derivative of L w.r.t. a[1]
+        self._dL_da = np.sum(self._dL_dy * dy_du * da, axis=0)
 
         return True
 
@@ -134,11 +196,58 @@ class BackProbOptimizer:
         if ( self.backward() ):
         
             if (self._use_adam):
-                # Use ADAM algorithm
-                pass
+                # updating a_i
+                self._vda = self._beta1*self._vda + (1-self._beta1)*self._dL_da
+                self._sda = self._beta2*self._sda + (1-self._beta2)*(self._dL_da**2)
+
+                vda_corrected = self._vda/(1.0-self._beta1**iter)
+                sda_corrected = self._sda/(1.0-self._beta2**iter)
+                new_a = self._a[1:] - self._alpha * vda_corrected/(np.sqrt(sda_corrected)+self._eps)
+                self._new_a = self._a
+                self._new_a[1:] = new_a
+
+                
+                # updating b_i
+                self._vdb = self._beta1*self._vdb + (1-self._beta1)*self._dL_db
+                self._sdb = self._beta2*self._sdb + (1-self._beta2)*(self._dL_db**2)
+
+                vdb_corrected = self._vdb/(1.0-self._beta1**iter)
+                sdb_corrected = self._sdb/(1.0-self._beta2**iter)
+                new_b = self._b - self._alpha * vdb_corrected/(np.sqrt(sdb_corrected)+self._eps)
+                self._new_b = new_b
             else:
-                # Use regular gradient descent algorithm
+                # TODO Use regular gradient descent algorithm
                 pass
+
+    def setSignals(self, r=None, u=None, y=None):
+        """Setter function for the system signals.
+        Updates self._r, self._u, self._y
+
+        @param r Array of system's reference signal
+        @param u Array of controller output signal
+        @param y Array of system's output signal
+        """
+
+        # Sanity checks
+        if (r is None):
+            print("[ERROR] [setSignals] reference signal r is None.")
+            return False
+        if (u is None):
+            print("[ERROR] [setSignals] Controller output signal u is None.")
+            return False
+        if (y is None):
+            print("[ERROR] [setSignals] System's output signal y is None.")
+            return False
+
+        self._r=r
+        self._u=u
+        self._y=y
+
+        if (self._debug):
+            print("[DEBUG] [setSignals] Reference signal , controller & system output signals are set.")
+            print("[DEBUG] [setSignals] Signal lengths: len(r)={}, len(u)={}, len(y)={}".format(len(self._r), len(self._u), len(self._y)))
+
+        return True
 
     def setU(self, u=None):
         """This is a setter function for class variable _u, controller output.
@@ -174,23 +283,23 @@ class BackProbOptimizer:
             
         return True
 
-    def setContCoeffs(self, a=None, b=None):
+    def setContCoeffs(self, den_coeff=None, num_coeff=None):
         """Setter function for controller coeffs a,b
 
         @return True if successful, False if not
         """
-        if (a is None):
-            print("[ERROR] [setContCoeffs] a is None.")
+        if (den_coeff is None):
+            print("[ERROR] [setContCoeffs] Denominator (den_coeff) is None.")
             return False
-        if (b is None):
-            print("[ERROR] [setContCoeffs] b is None.")
+        if (num_coeff is None):
+            print("[ERROR] [setContCoeffs] Numerator (num_coeff) is None.")
             return False
-        if (a[0] != 1):
-            print("[ERROR] [setContCoeffs] a[0] is not 1.")
+        if (den_coeff[0] != 1):
+            print("[ERROR] [setContCoeffs] den_coeff[0] should be 1.0 .")
             return False
 
-        self._a = a
-        self._b = b
+        self._a = den_coeff
+        self._b = num_coeff
 
     def getNewContCoeff(self):
         """Returns _new_a, _new_b
@@ -201,6 +310,16 @@ class BackProbOptimizer:
         """Return _dL_dy, _dL_du, _dL_da, _dL_db
         """
         return self._dL_dy, self._dL_du, self._dL_da, self._dL_db
+
+    def getError(self):
+        """Returns self._error
+        """
+        return self._error
+    
+    def getObjective(self):
+        """Returns self._objective
+        """
+        return self._objective
 
 def test():
     obj = BackProbOptimizer()
