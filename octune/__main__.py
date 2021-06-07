6068f46c84c5aa4b71243344eb25f9c90a8e9c44
Mohamed Abdelkader <mohamedashraf123@gmail.com>
2021-06-07 21:39:36 +0300

    Update __main__.py

--- octune/__main__.py
+++ octune/__main__.py
@@ -1,4 +1,4 @@
-from octune import optimization, control_system
+from octune import optimization, control_system, plot
 
 def main():
     print("\n[main] This is the main file of the OCTUNE package.\n")
